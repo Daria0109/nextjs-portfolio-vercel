bc4d20e280121b5ceaffe45524e04ebe559560ed
Darya Shnipova <dshnipova@gmail.com>
2023-01-08 03:07:25 +0100

    Removed variables from config

--- next.config.js
+++ next.config.js
@@ -1,26 +1,3 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-const nextConfig = (phase) => {
-	if (phase === PHASE_DEVELOPMENT_SERVER) {
-		return {
-			reactStrictMode: true,
-			env: {
-				mongodb_username: 'DaryaShnipava',
-				mongodb_password: '41CXIDronxeVtQgQ',
-				mongodb_cluster: 'cluster0',
-				mongodb_database: 'js-blog-dev'
-			}
-		};
-	}
-	return {
-		reactStrictMode: true,
-		env: {
-			mongodb_username: 'DaryaShnipava',
-			mongodb_password: '41CXIDronxeVtQgQ',
-			mongodb_cluster: 'cluster0',
-			mongodb_database: 'js-blog'
-		}
-	};
-};
+const nextConfig = { reactStrictMode: true };
 
 module.exports = nextConfig;
